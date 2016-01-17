3f30a9522dc919d3c7b64b20ca146018d30ff425
Ryan Weaver <ryan@thatsquality.com>
2016-01-17 14:20:18 -0500

    Fixing test for 2.3 compat

--- Tests/DependencyInjection/KnpUOAuth2ClientExtensionTest.php
+++ Tests/DependencyInjection/KnpUOAuth2ClientExtensionTest.php
@@ -40,12 +40,25 @@ class KnpUOAuth2ClientExtensionTest extends \PHPUnit_Framework_TestCase
         $loader->load(array($config), $this->configuration);
 
         $definition = $this->configuration->getDefinition('knpu.oauth2.facebook_client');
-        $factory = $definition->getFactory();
-        // make sure the factory is correct
-        $this->assertEquals(
-            array(new Reference('knpu.oauth.provider_factory'), 'createProvider'),
-            $factory
-        );
+
+        if (method_exists($definition, 'getFactory')) {
+            $factory = $definition->getFactory();
+            // make sure the factory is correct
+            $this->assertEquals(
+                array(new Reference('knpu.oauth.provider_factory'), 'createProvider'),
+                $factory
+            );
+        } else {
+            // 2.3-2.5 compat
+            $factoryService = $definition->getFactoryService();
+            // make sure the factory is correct
+            $this->assertEquals(
+                'knpu.oauth.provider_factory',
+                $factoryService
+            );
+            $this->assertEquals('createProvider', $definition->getFactoryMethod());
+        }
+
         $this->assertEquals(
             array(
                 'League\OAuth2\Client\Provider\Facebook',
