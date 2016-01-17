476e88898821763b5641bdee85162452931892ae
Ryan Weaver <ryan@thatsquality.com>
2016-01-17 14:42:38 -0500

    Also overriding the method in the bundle to allow for the alias
    ... we should really change this...

--- KnpUOAuth2ClientBundle.php
+++ KnpUOAuth2ClientBundle.php
@@ -2,9 +2,22 @@
 
 namespace KnpU\OAuth2ClientBundle;
 
+use KnpU\OAuth2ClientBundle\DependencyInjection\KnpUOAuth2ClientExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class KnpUOAuth2ClientBundle extends Bundle
 {
+    /**
+     * Overridden to allow for the custom extension alias.
+     *
+     * @return KnpUOAuth2ClientExtension
+     */
+    public function getContainerExtension()
+    {
+        if (null === $this->extension) {
+            return new KnpUOAuth2ClientExtension();
+        }
 
+        return $this->extension;
+    }
 }
