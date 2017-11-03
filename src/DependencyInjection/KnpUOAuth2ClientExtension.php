c7e1bd7cb4abe566760aa3f2e12deb6638225873
Ryan Weaver <ryan@thatsquality.com>
2017-11-03 13:19:55 -0400

    Making services explicitly public so that they remain public in Symfony 4

--- src/DependencyInjection/KnpUOAuth2ClientExtension.php
+++ src/DependencyInjection/KnpUOAuth2ClientExtension.php
@@ -222,6 +222,7 @@ class KnpUOAuth2ClientExtension extends Extension
             new Reference($providerServiceKey),
             new Reference('request_stack'),
         ]);
+        $clientDefinition->setPublic(true);
 
         // if stateless, do it!
         if (!$useState) {
