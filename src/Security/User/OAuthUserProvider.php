753f99eb6271e1789ce97233345ecfcc766667b3
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-01-03 10:22:35 +0000

    Add simple user provider

---
+++ src/Security/User/OAuthUserProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * OAuth2 Client Bundle
+ * Copyright (c) KnpUniversity <http://knpuniversity.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace KnpU\OAuth2ClientBundle\Security\User;
+
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+class OAuthUserProvider implements UserProviderInterface
+{
+    private $roles;
+
+    public function __construct(array $roles = ['ROLE_USER', 'ROLE_OAUTH_USER'])
+    {
+        $this->roles = $roles;
+    }
+
+    public function loadUserByUsername($username)
+    {
+        return new OAuthUser($username, $this->roles);
+    }
+
+    public function refreshUser(UserInterface $user)
+    {
+        if (!$user instanceof OAuthUser) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', get_class($user)));
+        }
+
+        return $this->loadUserByUsername($user->getUsername());
+    }
+
+    public function supportsClass($class)
+    {
+        return OAuthUser::class === $class;
+    }
+}
