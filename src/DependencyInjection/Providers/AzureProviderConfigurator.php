e46756138ef926e1237e9f172e751d6ccf67ae25
Alexander Eimer <alexander.eimer@gmail.com>
2018-10-04 11:42:57 +0200

    Adds whole config for azure

--- src/DependencyInjection/Providers/AzureProviderConfigurator.php
+++ src/DependencyInjection/Providers/AzureProviderConfigurator.php
@@ -16,7 +16,34 @@ class AzureProviderConfigurator implements ProviderConfiguratorInterface
 {
     public function buildConfiguration(NodeBuilder $node)
     {
-        // no custom options
+        $node
+            ->scalarNode('url_login')
+                ->info('Domain to build login URL')
+            ->end()
+            ->scalarNode('path_authorize')
+                ->info('Oauth path to authorize against')
+            ->end()
+            ->scalarNode('path_token')
+                ->info('Oauth path to retrieve a token')
+            ->end()
+            ->arrayNode('scope')
+                ->info('Oauth scope send with the request')
+            ->end()
+            ->scalarNode('tenant')
+                ->info('The tenant to use, default is `common`')
+            ->end()
+            ->scalarNode('url_api')
+                ->info('Domain to build request URL')
+            ->end()
+            ->scalarNode('resource')
+                ->info('Oauth resource field')
+            ->end()
+            ->scalarNode('api_version')
+                ->info('The API version to run against')
+            ->end()
+            ->booleanNode('auth_with_resource')
+                ->info('Send resource field with auth-request')
+            ->end()
     }
 
     public function getProviderClass(array $config)
@@ -27,8 +54,17 @@ class AzureProviderConfigurator implements ProviderConfiguratorInterface
     public function getProviderOptions(array $config)
     {
         return [
-            'clientId' => $config['client_id'],
-            'clientSecret' => $config['client_secret'],
+            'clientId'         => $config['client_id'],
+            'clientSecret'     => $config['client_secret'],
+            'urlLogin'         => $config['url_login'],
+            'pathAuthorize'    => $config['path_authorize'],
+            'pathToken'        => $config['path_token'],
+            'scope'            => $config['scope'],
+            'tenant'           => $config['tenant'],
+            'urlAPI'           => $config['url_api'],
+            'resource'         => $config['resource'],
+            'API_VERSION'      => $config['api_version'],
+            'authWithResource' => $config['auth_with_resource'],
         ];
     }
 
