6021e0798f140a7f8d658843f66a742e0ecfd3d0
Ryan Weaver <ryan@thatsquality.com>
2020-08-30 12:20:51 -0400

    improving docs about redirect options on concrete class

--- src/Client/OAuth2Client.php
+++ src/Client/OAuth2Client.php
@@ -54,7 +54,9 @@ class OAuth2Client implements OAuth2ClientInterface
      * OAuth2 server (e.g. send them to Facebook).
      *
      * @param array $scopes  The scopes you want (leave empty to use default)
-     * @param array $options Extra options to pass to the "Provider" class
+     * @param array $options Extra options to pass to the Provider's getAuthorizationUrl()
+     *                       method. For example, <code>scope</code> is a common option.
+     *                       Generally, these become query parameters when redirecting.
      *
      * @return RedirectResponse
      */
