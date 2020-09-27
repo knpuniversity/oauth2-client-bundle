3fb9f9856ec72d86b480a1a77969de03b9588494
Ed Walker <github@foxwire.org>
2020-09-26 22:45:16 -0700

    Separate mock creation, fix typo.

--- tests/Client/Provider/BatchProviderTest.php
+++ tests/Client/Provider/BatchProviderTest.php
@@ -22,28 +22,45 @@ class BatchProviderTest extends TestCase
 {
     public function testProviders()
     {
-        // This is basically just validating that the clients are sane/implemeting OAuth2Client
+        // This is basically just validating that the clients are sane/implementing OAuth2Client
 
         $mockAccessToken = $this->getMockBuilder(AccessToken::class)->disableOriginalConstructor()->getMock();
-        $mockProvider = $this->getMockBuilder(AbstractProvider::class)->getMock();
-        $mockProvider->method("getResourceOwner")->willReturn($this->getMockBuilder(ResourceOwnerInterface::class)->getMock());
-        $mockProvider->method("getAccessToken")->willReturn($mockAccessToken);
-        $mockRequest = $this->getMockBuilder(Request::class)->getMock();
-        $mockRequest->method("get")->willReturn(true);
-        $mockRequestStack = $this->getMockBuilder(RequestStack::class)->getMock();
-        $mockRequestStack->method("getCurrentRequest")->willReturn($mockRequest);
+        $mockProvider = $this->getMockProvider($mockAccessToken);
+        $mockRequestStack = $this->getMockRequestStack($this->getMockRequest());
 
         $clients = scandir(__DIR__ . "/../../../src/Client/Provider");
         foreach($clients as $client) {
             if(substr($client, -4, 4) !== ".php") { continue; }
 
             $client = sprintf("KnpU\OAuth2ClientBundle\Client\Provider\%s", explode(".", $client)[0]);
-            $testProvider = new $client($mockProvider, $mockRequestStack);
-            $testProvider->setAsStateless();
-            $this->assertTrue(is_subclass_of($testProvider, OAuth2Client::class));
+            $testClient = new $client($mockProvider, $mockRequestStack);
+            $testClient->setAsStateless();
+            $this->assertTrue(is_subclass_of($testClient, OAuth2Client::class));
 
-            $this->assertInstanceOf(ResourceOwnerInterface::class, $testProvider->fetchUserFromToken($mockAccessToken));
-            $this->assertInstanceOf(ResourceOwnerInterface::class, $testProvider->fetchUser());
+            $this->assertInstanceOf(ResourceOwnerInterface::class, $testClient->fetchUserFromToken($mockAccessToken));
+            $this->assertInstanceOf(ResourceOwnerInterface::class, $testClient->fetchUser());
         }
+    }
+
+    private function getMockProvider($mockAccessToken)
+    {
+        $mockProvider = $this->getMockBuilder(AbstractProvider::class)->getMock();
+        $mockProvider->method("getResourceOwner")->willReturn($this->getMockBuilder(ResourceOwnerInterface::class)->getMock());
+        $mockProvider->method("getAccessToken")->willReturn($mockAccessToken);
+        return $mockProvider;
+    }
+
+    private function getMockRequest()
+    {
+        $mockRequest = $this->getMockBuilder(Request::class)->getMock();
+        $mockRequest->method("get")->willReturn(true);
+        return $mockRequest;
+    }
+
+    private function getMockRequestStack($mockRequest)
+    {
+        $mockRequestStack = $this->getMockBuilder(RequestStack::class)->getMock();
+        $mockRequestStack->method("getCurrentRequest")->willReturn($mockRequest);
+        return $mockRequestStack;
     }
 }
