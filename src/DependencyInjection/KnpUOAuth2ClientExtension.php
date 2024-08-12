2f50bcfcb9a6df9c34b320bb494cc7f0fc436ff9
Evgeny <xonix75@mail.ru>
2024-08-12 18:08:25 +0300

    fix-for-new-symfony (#449)

--- src/DependencyInjection/KnpUOAuth2ClientExtension.php
+++ src/DependencyInjection/KnpUOAuth2ClientExtension.php
@@ -82,7 +82,7 @@ use Symfony\Component\DependencyInjection\Alias;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
 class KnpUOAuth2ClientExtension extends Extension
 {
