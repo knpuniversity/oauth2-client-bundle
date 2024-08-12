0f8db87efa064bc1800315c027a80b53ef935524
Victor Bocharsky <bocharsky.bw@gmail.com>
2024-08-12 17:26:07 +0200

    Fix CS (#450)
    * Fix CS
    
    * Bring back the use statement - just reorder
    
    * Fix missing semicolon

--- src/DependencyInjection/KnpUOAuth2ClientExtension.php
+++ src/DependencyInjection/KnpUOAuth2ClientExtension.php
@@ -80,9 +80,9 @@ use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Alias;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\DependencyInjection\Extension\Extension;
 
 class KnpUOAuth2ClientExtension extends Extension
 {
